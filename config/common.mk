90ecf2f817bf9a0a4d24027526ea4dab69eb795e
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-20 15:49:42 +0530

    vendor: Introduce overlay for Lawnchair default conf
    Change-Id: Ie05eaea58ed341d0f4f2f5dcc177663afe401de1
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -107,6 +107,15 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
     vendor/havoc/prebuilt/common/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.potato.overlay.lawnconf
+
+# Lawnchair Default Configuration
+ifeq ($(LAWNCHAIR_OPTOUT),)
+PRODUCT_PACKAGES += \
+    LawnConf
+endif
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
