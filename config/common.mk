5e4f83f068fb8e0412b62a6182f019e883547b3d
Singhaarav059 <Singhaarav059@gmail.com>
2020-02-23 10:57:36 +0000

    fix

--- config/common.mk
+++ config/common.mk
@@ -112,10 +112,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.vendor.overlay.theme=com.potato.overlay.lawnconf
 
 # Lawnchair Default Configuration
-ifeq ($(LAWNCHAIR_OPTOUT),)
 PRODUCT_PACKAGES += \
     LawnConf
-endif
 
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
