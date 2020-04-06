bf4b107938aceb82bad4f64c45dc544e4404c7e6
Singhaarav059 <Singhaarav059@gmail.com>
2020-04-06 05:34:08 +0000

    Revert vendor: Convert NexusLauncherReleaseOverlay to a static overlay"
    This reverts commit 8eb0b421c6a7d6b7b4c99317ab390e3e0290c3ed.

--- packages/overlays/NexusLauncherReleaseOverlay/Android.mk
+++ packages/overlays/NexusLauncherReleaseOverlay/Android.mk
@@ -1,15 +1,11 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := NexusLauncher
-
-LOCAL_PRODUCT_MODULE := true
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := NexusLauncherReleaseOverlay
 LOCAL_SDK_VERSION := current
-
-include $(BUILD_RRO_PACKAGE)
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+LOCAL_PRODUCT_MODULE := true
+include $(BUILD_PACKAGE)
