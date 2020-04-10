3d50379e274118c43cbdeba5573e9dba6c14c39a
ashwatthama <sai404142@gmail.com>
2020-04-10 09:11:13 +0000

    Xtended :Add more fonts [1/2]
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- config/fonts.mk
+++ config/fonts.mk
@@ -7,4 +7,12 @@ PRODUCT_COPY_FILES += \
     vendor/havoc/prebuilt/common/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
     vendor/havoc/prebuilt/common/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/havoc/prebuilt/common/fonts/CircularStd.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/CircularStd.ttf \
-    vendor/havoc/prebuilt/common/fonts/OnePlusSlate.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/OnePlusSlate.ttf
+    vendor/havoc/prebuilt/common/fonts/OnePlusSlate.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/OnePlusSlate.ttf \
+    vendor/havoc/prebuilt/common/fonts/AntipastoPro/AntipastoPro.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/AntipastoPro.ttf \
+    vendor/havoc/prebuilt/common/fonts/EvolveSans/EvolveSans.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/EvolveSans.ttf \
+    vendor/havoc/prebuilt/common/fonts/Fucek/Fucek.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Fucek.ttf \
+    vendor/havoc/prebuilt/common/fonts/LemonMilk/LemonMilk.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/LemonMilk.ttf \
+    vendor/havoc/prebuilt/common/fonts/Oduda/Oduda.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Oduda.ttf \
+    vendor/havoc/prebuilt/common/fonts/ReemKufi/ReemKufi.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/ReemKufi.ttf \
+    vendor/havoc/prebuilt/common/fonts/GoogleSansMedium/GoogleSansMedium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSansMedium.ttf \
+    vendor/havoc/prebuilt/common/fonts/SimpleDay/SimpleDay.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SimpleDay.ttf 
